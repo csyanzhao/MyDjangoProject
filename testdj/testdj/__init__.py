aa5125791af9da568beeb332a0acfb5b580bfdb4
csyanzhao@126.com <csyanzhao@126.com>
2019-12-25 11:06:02 +0800

    test

---
+++ testdj/testdj/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: ZhaoYan
+# @Date:   2019-12-19 14:35:08
+# @Last Modified by:   ZhaoYan
+# @Last Modified time: 2019-12-24 15:19:11
+import pymysql
+pymysql.install_as_MySQLdb()
